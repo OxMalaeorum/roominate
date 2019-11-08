45bb63c276b56391d5dfc13fbac991264168ccd8
oxmalaeorum <oxmalaeorum@gmail.com>
2019-11-08 13:43:13 +0800

    Update applicHome view

---
+++ src/views/ApplicantHome.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout row wrap mt-5 mx-2>
+    <v-flex>
+      <h1>Applicant Home</h1>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      applicant: {}
+    };
+  }
+};
+</script>
